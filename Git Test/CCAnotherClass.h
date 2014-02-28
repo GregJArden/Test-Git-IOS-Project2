549c1881902133a32ce830a7051cd5571422b17a
Greg Arden <greg.arden@talktalk.net>
2014-02-28 12:55:32 +0000

    added a test class

---
+++ Git Test/CCAnotherClass.h
@@ -0,0 +1,13 @@
+//
+//  CCAnotherClass.h
+//  Git Test
+//
+//  Created by Gregory Arden on 28/02/2014.
+//  Copyright (c) 2014 Not Defined. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCAnotherClass : NSObject
+
+@end
---
+++ Git Test/CCAnotherClass.m
@@ -0,0 +1,13 @@
+//
+//  CCAnotherClass.m
+//  Git Test
+//
+//  Created by Gregory Arden on 28/02/2014.
+//  Copyright (c) 2014 Not Defined. All rights reserved.
+//
+
+#import "CCAnotherClass.h"
+
+@implementation CCAnotherClass
+
+@end
